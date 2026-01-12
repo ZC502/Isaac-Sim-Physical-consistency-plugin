2ed59bac6a41c1f3493ba4cc304f7b32d70f7369
ZC502 <liouzuocen@qq.com>
2026-01-12 09:40:06 +0800

    Update octonion.py

--- octonion_time/octonion.py
+++ octonion_time/octonion.py
@@ -2,19 +2,94 @@ import numpy as np
 
 class Octonion:
     def __init__(self, r=1.0, i=None):
-        self.r = r
-        self.i = np.zeros(7) if i is None else np.array(i)
+        self.r = float(r)
+        self.i = np.zeros(7, dtype=np.float64) if i is None else np.array(i, dtype=np.float64)
+
+    def copy(self):
+        return Octonion(self.r, self.i.copy())
 
     def normalize(self):
-        n = np.sqrt(self.r**2 + np.dot(self.i, self.i))
+        n = np.sqrt(self.r * self.r + np.dot(self.i, self.i))
         if n > 0:
             self.r /= n
             self.i /= n
 
     def __mul__(self, other):
-        # 第一版：可以先写成 placeholder
-        # 第二版：严格复现你 C 里的 octonion_mult
-        c = Octonion()
-        c.r = self.r * other.r - np.dot(self.i, other.i)
-        # TODO: 完整虚部乘法（直接照抄你的 C）
+        """
+        Strict translation of your C octonion_mult()
+        Non-associative, non-commutative by construction
+        """
+        a = self
+        b = other
+        c = Octonion(0.0)
+
+        # ---------- real part ----------
+        c.r = (
+            a.r * b.r
+            - a.i[0]*b.i[0] - a.i[1]*b.i[1] - a.i[2]*b.i[2]
+            - a.i[3]*b.i[3] - a.i[4]*b.i[4] - a.i[5]*b.i[5]
+            - a.i[6]*b.i[6]
+        )
+
+        # ---------- imaginary parts ----------
+        # e0
+        c.i[0] = (
+            a.r*b.i[0] + a.i[0]*b.r
+            + (a.i[1]*b.i[2] - a.i[2]*b.i[1])
+            + (a.i[5]*b.i[6] - a.i[6]*b.i[5])
+            - (a.i[3]*b.i[4] - a.i[4]*b.i[3])
+        )
+
+        # e1
+        c.i[1] = (
+            a.r*b.i[1] + a.i[1]*b.r
+            + (a.i[2]*b.i[0] - a.i[0]*b.i[2])
+            + (a.i[3]*b.i[5] - a.i[5]*b.i[3])
+            - (a.i[4]*b.i[6] - a.i[6]*b.i[4])
+        )
+
+        # e2
+        c.i[2] = (
+            a.r*b.i[2] + a.i[2]*b.r
+            + (a.i[0]*b.i[1] - a.i[1]*b.i[0])
+            + (a.i[4]*b.i[5] - a.i[5]*b.i[4])
+            - (a.i[3]*b.i[6] - a.i[6]*b.i[3])
+        )
+
+        # e3  (rotation–translation coupling)
+        c.i[3] = (
+            a.r*b.i[3] + a.i[3]*b.r
+            + (a.i[0]*b.i[5] - a.i[5]*b.i[0])
+            + (a.i[1]*b.i[4] - a.i[4]*b.i[1])
+            + (a.i[6]*b.i[2] - a.i[2]*b.i[6])
+            + (a.i[0]*b.i[2] - a.i[2]*b.i[0])
+            - (a.i[1]*b.i[0] - a.i[0]*b.i[1])
+        )
+
+        # e4
+        c.i[4] = (
+            a.r*b.i[4] + a.i[4]*b.r
+            + (a.i[3]*b.i[0] - a.i[0]*b.i[3])
+            + (a.i[1]*b.i[6] - a.i[6]*b.i[1])
+            - (a.i[2]*b.i[5] - a.i[5]*b.i[2])
+        )
+
+        # e5
+        c.i[5] = (
+            a.r*b.i[5] + a.i[5]*b.r
+            + (a.i[6]*b.i[0] - a.i[0]*b.i[6])
+            + (a.i[3]*b.i[1] - a.i[1]*b.i[3])
+            + (a.i[2]*b.i[4] - a.i[4]*b.i[2])
+        )
+
+        # e6 (disturbance coupling)
+        c.i[6] = (
+            a.r*b.i[6] + a.i[6]*b.r
+            + (a.i[0]*b.i[4] - a.i[4]*b.i[0])
+            + (a.i[1]*b.i[5] - a.i[5]*b.i[1])
+            + (a.i[3]*b.i[2] - a.i[2]*b.i[3])
+            + (a.i[0]*b.i[1] - a.i[1]*b.i[0])
+            - (a.i[2]*b.i[3] - a.i[3]*b.i[2])
+        )
+
         return c
